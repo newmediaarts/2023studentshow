6117169b849c4f0db073cb202188d4f91177a643
lummelis <lummelis@hawaii.edu>
2023-05-14 01:19:51 -1000

    update img

--- ui-website-test12/js/script.js
+++ ui-website-test12/js/script.js
@@ -1,33 +1,37 @@
 Fancybox.bind('[data-fancybox="gallery"]', {
     //
-});Fancybox.bind('[data-fancybox="orion"]', {
+}); Fancybox.bind('[data-fancybox="gallery-orion"]', {
     //
-}); Fancybox.bind('[data-fancybox="yuma"]', {
+}); Fancybox.bind('[data-fancybox="gallery-yuma"]', {
     //
-}); Fancybox.bind('[data-fancybox="kimi"]', {
+}); Fancybox.bind('[data-fancybox="gallery-kimi"]', {
     //
-}); Fancybox.bind('[data-fancybox="selena"]', {
+}); Fancybox.bind('[data-fancybox="gallery-selena"]', {
     //
-}); Fancybox.bind('[data-fancybox="kaleb"]', {
+}); Fancybox.bind('[data-fancybox="gallery-kaleb"]', {
     //
-}); Fancybox.bind('[data-fancybox="cass"]', {
+}); Fancybox.bind('[data-fancybox="gallery-cass"]', {
     //
-}); Fancybox.bind('[data-fancybox="kanalu"]', {
+}); Fancybox.bind('[data-fancybox="gallery-kanalu"]', {
     //
-}); Fancybox.bind('[data-fancybox="troy"]', {
+}); Fancybox.bind('[data-fancybox="gallery-troy"]', {
     //
-}); Fancybox.bind('[data-fancybox="maria"]', {
+}); Fancybox.bind('[data-fancybox="gallery-maria"]', {
     //
-}); Fancybox.bind('[data-fancybox="vinsent"]', {
+}); Fancybox.bind('[data-fancybox="gallery-vinsent"]', {
     //
-}); Fancybox.bind('[data-fancybox="hiroaki"]', {
+}); Fancybox.bind('[data-fancybox="gallery-hiroaki"]', {
     //
-}); Fancybox.bind('[data-fancybox="mikaela"]', {
+}); Fancybox.bind('[data-fancybox="gallery-mikaela"]', {
     //
-}); Fancybox.bind('[data-fancybox="tre"]', {
+}); Fancybox.bind('[data-fancybox="gallery-tre"]', {
     //
-}); Fancybox.bind('[data-fancybox="jenny"]', {
+}); Fancybox.bind('[data-fancybox="gallery-jenny"]', {
     //
-});Fancybox.bind('[data-fancybox="video-gallery"]', {
+}); Fancybox.bind('[data-fancybox="video-gallery"]', {
     //
-});
+}); Fancybox.defaults.Hash = false;
+
+const options = {
+    Hash: false,
+};
